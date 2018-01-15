0de5db4047663fa153cfe1954216fe096bddb8f8
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-15 17:39:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function nameToAttributes(drivers) {
 
 function attributesToPhrase(drivers) {
   return drivers.map(function (driver) {
-    return console.log(driver.name + " is from " + driver.hometown)
+    return driver.name + " is from " + driver.hometown
   })
 }
