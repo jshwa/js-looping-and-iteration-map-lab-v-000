4aece74b825a314ec8bdc1a87f1b54e9b9736e2c
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-15 17:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   return drivers.map(function (driver) {
-    return driver.filter(function (char) {
+    return driver.map(function (char) {
       char === " "
       console.log(char)
     })
